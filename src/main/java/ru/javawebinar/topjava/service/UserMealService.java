109f91e4a3613bdebc626a79441f6a7eeb4c3ea8
VladimirTsypanov <v.tsipanov@hotmail.com>
2016-03-20 11:31:24 +0200

    HW2

--- src/main/java/ru/javawebinar/topjava/service/UserMealService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserMealService.java
@@ -1,8 +1,26 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.UserMeal;
+
+import java.time.LocalDate;
+import java.util.Collection;
+
 /**
  * GKislin
  * 15.06.2015.
  */
 public interface UserMealService {
+
+    UserMeal save(UserMeal userMeal, Integer userId);
+
+    UserMeal update(UserMeal userMeal, Integer userId);
+
+    void delete(int id, Integer userId);
+
+    UserMeal get(int id, Integer userId);
+
+    Collection<UserMeal> getAll(Integer userId);
+
+    Collection<UserMeal> getFiltered(LocalDate startDate, LocalDate endDate, int userId);
+
 }
